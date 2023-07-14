9189932d622796355197dd1ed8171505d965e8e6
Augusto Villar <augusto.villar@usp.br>
2023-07-14 13:30:35 -0300

    corrige erro do j

--- Memoria.cpp
+++ Memoria.cpp
@@ -1,41 +1,48 @@
 
 #include <cstring>
 
-class Memoria {
+class Memoria
+{
 
 private:
     int tamanho_memoria;
     int *mapa_de_bits;
 
 public:
-
-    Memoria(int tamanho_memoria = 20) {
+    Memoria(int tamanho_memoria = 20)
+    {
         this->tamanho_memoria = tamanho_memoria;
         this->mapa_de_bits = new int[tamanho_memoria];
         memset(this->mapa_de_bits, 0, tamanho_memoria);
     }
 
-    bool alocaProcesso (int tamanho_desejado, int PID){
+    bool alocaProcesso(int tamanho_desejado, int PID)
+    {
         bool alocouProcesso = NULL;
-        
-        //passa a memoria inteira
-        for(int i = 0; i < (this->tamanho_memoria); i++){
-            if(this->mapa_de_bits[i] == 0){
-                int j=i; 
-                //verifica se o processo cabe naquele espaço
-                while(mapa_de_bits[j]== 0 && (i-j+1)<tamanho_desejado && j+1<this->tamanho_memoria){
+
+        // passa a memoria inteira
+        for (int i = 0; i < (this->tamanho_memoria); i++)
+        {
+            if (this->mapa_de_bits[i] == 0)
+            {
+                int j = i;
+                // verifica se o processo cabe naquele espaço
+                while (mapa_de_bits[j] == 0 && (j - i + 1) < tamanho_desejado && j + 1 < this->tamanho_memoria)
+                {
                     j++;
                 }
 
-                //caso caiba, coloca PID no mapa de bits
-                if (i-j+1 == tamanho_memoria){
-                    for(int k=j;k-j<tamanho_desejado;k++){
+                // caso caiba, coloca PID no mapa de bits
+                if (j - i + 1 == tamanho_memoria)
+                {
+                    for (int k = j; k - j < tamanho_desejado; k++)
+                    {
                         mapa_de_bits[k] = PID;
                     }
                     return 1;
                 }
             }
-        } 
+        }
 
         return alocouProcesso;
     }
